852df99b1a02d440acaef50e52e2a83e1b7c3002
Alejandro Avila <alejandroaperez1994g@gmail.com>
2022-05-03 16:14:44 +0100

    refactor submitEventForm()

--- src/js/validation.js
+++ src/js/validation.js
@@ -1,83 +1,82 @@
 import { formEvent } from "./main.js";
 
-import {
-    Event
-  } from "./events.js";
+import { Event } from "./events.js";
 
-import {
-    saveEvents
-} from "./localstorage.js";
+import { saveEvents } from "./localstorage.js";
 
-  formEvent.elements['checkbox__endDate'].addEventListener("change", () => {
-    const inputDateEnd = formEvent.querySelector('label[for=end]');
-    inputDateEnd.classList.toggle('hide');
-  });
-  
-  formEvent.elements['checkbox__reminder'].addEventListener("change", () => {
-    const reminder = formEvent.querySelector('label[for=select__reminder]');
-    reminder.classList.toggle('hide');
-  });
-  
-  formEvent.addEventListener("submit", submitEventForm);
-  
-  function submitEventForm(e) {
-    e.preventDefault();
-    const title = formEvent.elements["title"];
-    const endDateCheck = formEvent.elements["checkbox__endDate"];
-    const reminderCheck = formEvent.elements["checkbox__reminder"];
-    const reminder = formEvent.elements["select__reminder"];
-    const description = formEvent.elements["description"];
-    const type = formEvent.elements["select__eventType"];
-    const initialLabel = formEvent.querySelector('label[for="initial"]');
-    const endLabel = formEvent.querySelector('label[for="end"]');
-  
-    const initDate = new Date(
-      `${formEvent.elements["initial__date"].value}T${formEvent.elements["initial__time"].value}`
-    );
-  
-    const endDate = new Date(
-      `${formEvent.elements["end__date"].value}T${formEvent.elements["end__time"].value}`
-    );
-  
-    let newEvent = new Event(title.value, description.value, initDate, endDateCheck, endDate, reminderCheck, reminder.value, type.value);
-  
-  
-    //Input required
-    if (newEvent.checkRequiredInputs()) {
-      addErrorMessage(title, newEvent.requiredMessages("Title"));
+function submitEventForm(e) {
+  e.preventDefault();
+  const title = formEvent.elements["title"];
+  const endDateCheck = formEvent.elements["checkbox__endDate"];
+  const reminderCheck = formEvent.elements["checkbox__reminder"];
+  const reminder = formEvent.elements["select__reminder"];
+  const description = formEvent.elements["description"];
+  const type = formEvent.elements["select__eventType"];
+  const initialLabel = formEvent.querySelector('label[for="initial"]');
+  const endLabel = formEvent.querySelector('label[for="end"]');
+
+  const initDate = new Date(
+    `${formEvent.elements["initial__date"].value}T${formEvent.elements["initial__time"].value}`
+  );
+
+  const endDate = new Date(
+    `${formEvent.elements["end__date"].value}T${formEvent.elements["end__time"].value}`
+  );
+
+  let newEvent = new Event(
+    title.value,
+    description.value,
+    initDate,
+    endDateCheck,
+    endDate,
+    reminderCheck,
+    reminder.value,
+    type.value
+  );
+
+  //Input required
+  if (newEvent.checkRequiredInputs()) {
+    addErrorMessage(title, newEvent.requiredMessages("Title"));
+  } else {
+    // Validate title
+    if (newEvent.checkTitle(title.value.length)) {
+      addErrorMessage(title, newEvent.errorMessages(title.name));
     } else {
-       // Validate title
-      if (newEvent.checkTitle(title.value.length)) {  
-        addErrorMessage(title, newEvent.errorMessages(title.name));
-      } else {
-        removeErrorMessage(title);
-      }
+      removeErrorMessage(title);
     }
-  
-    //Validate Initial Date
-    if (newEvent.checkInitDate()) {
-      console.log('entra');
-      addErrorMessage(initialLabel, newEvent.errorMessages("initDate"));
-    } else {
-      removeErrorMessage(initialLabel);
-    }
-  
-    //Validate End Date
-    if (endDateCheck.checked) {
-      if (newEvent.checkEndDate()) {
-        addErrorMessage(endLabel, newEvent.errorMessages("endDate"));
-      } else {
-        removeErrorMessage(endLabel);
-      }
-    }
-  
-    if (!hasErrorMessages(formEvent)) {
-      //add to calendar
-
-    };
-  
   }
-  
+
+  //Validate Initial Date
+  if (newEvent.checkInitDate()) {
+    console.log("entra");
+    addErrorMessage(initialLabel, newEvent.errorMessages("initDate"));
+  } else {
+    removeErrorMessage(initialLabel);
+  }
+
+  //Validate End Date
+  if (endDateCheck.checked) {
+    if (newEvent.checkEndDate()) {
+      addErrorMessage(endLabel, newEvent.errorMessages("endDate"));
+    } else {
+      removeErrorMessage(endLabel);
+    }
+  }
+
+  if (!hasErrorMessages(formEvent)) {
+    const dateDifference = endDate.getTime() - initDate.getTime();
+    const numOfDays = Math.floor(dateDifference / (24 * 60 * 60 * 1000));
+    const month = initDate.getMonth();
+    const day = initDate.getDate();
+    const year = initDate.getFullYear();
+
+    if (endDateCheck.checked) {
+      saveEvents(year, month, day, newEvent, numOfDays);
+    } else {
+      saveEvents(year, month, day, newEvent);
+    }
+  }
+
   //Add div error message
   function addErrorMessage(input, message) {
     if (hasErrorMessage(input)) {
@@ -88,7 +87,7 @@ import {
       input.insertAdjacentElement("afterend", error);
     }
   }
-  
+
   //Remove div error message
   function removeErrorMessage(input) {
     if (hasErrorMessage(input)) {
@@ -96,17 +95,17 @@ import {
       input.nextElementSibling.remove();
     }
   }
-  
+
   //Check if input has error message
   function hasErrorMessage(input) {
     return input.classList.contains("invalid");
   }
-  
+
   //Check if form has error messages
   function hasErrorMessages(form) {
     return form.getElementsByClassName("invalid").length > 0;
   }
-  
+
   //Create div error
   function createErrorMessage(message) {
     let div = document.createElement("div");
@@ -116,11 +115,14 @@ import {
     div.appendChild(p);
     return div;
   }
-  
+
   function resetForm() {
     formEvent.querySelector('label[for="end"]').classList.add("hide");
-    formEvent.querySelector('select__reminder]').classList.add("hide");
+    formEvent.querySelector("select__reminder]").classList.add("hide");
     formEvent.reset();
-    const invalidInputs = formEvent.querySelector('.invalid');
+    const invalidInputs = formEvent.querySelector(".invalid");
     invalidInputs.forEach(removeErrorMessage);
   }
+}
+
+export { submitEventForm };
