c63fcb4f65a5bb8e96e78e6467171bccba70dc45
Alejandro Avila <alejandroaperez1994g@gmail.com>
2022-05-05 11:57:52 +0100

    refactor nextMonth() and previousMonth()

--- src/js/calendarController.js
+++ src/js/calendarController.js
@@ -1,19 +1,45 @@
+import {
+  year,
+  calendar,
+  buildCalendar,
+  numberOfMonths,
+  getFirstDayOfMonth,
+} from "./main.js";
+
+let numberOfYears = 0;
+
 const nextMonth = (currentMonth) => {
   if (currentMonth.nextElementSibling) {
-    currentMonth.classList.toggle("hidden");
-    currentMonth.removeAttribute("currentmonth");
-    currentMonth.nextElementSibling.classList.toggle("hidden");
-    currentMonth.nextElementSibling.setAttribute("currentmonth", "current");
+    nextNavigation(currentMonth);
+  } else {
+    numberOfYears++;
+    const nextYear = calendar(year + numberOfYears);
+    buildCalendar(nextYear);
+    getFirstDayOfMonth(year + numberOfYears, numberOfMonths);
+    nextNavigation(currentMonth);
   }
 };
 
 const previousMonth = (currentMonth) => {
   if (currentMonth.previousElementSibling) {
-    currentMonth.classList.toggle("hidden");
-    currentMonth.removeAttribute("currentmonth");
-    currentMonth.previousElementSibling.classList.toggle("hidden");
-    currentMonth.previousElementSibling.setAttribute("currentmonth", "current");
+    previousNavigation(currentMonth);
+  } else {
+    let parent = currentMonth.parentNode;
   }
+};
+
+const nextNavigation = (currentMonth) => {
+  currentMonth.classList.toggle("hide");
+  currentMonth.removeAttribute("currentmonth");
+  currentMonth.nextElementSibling.classList.toggle("hide");
+  currentMonth.nextElementSibling.setAttribute("currentmonth", "current");
+};
+
+const previousNavigation = (currentMonth) => {
+  currentMonth.classList.toggle("hide");
+  currentMonth.removeAttribute("currentmonth");
+  currentMonth.previousElementSibling.classList.toggle("hide");
+  currentMonth.previousElementSibling.setAttribute("currentmonth", "current");
 };
 
 export { nextMonth, previousMonth };
