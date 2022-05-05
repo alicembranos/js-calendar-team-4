c705158974bd0ab7a0e9d87e16d64612aaf9eb61
Alicia Cembranos <alicembru15@gmail.com>
2022-05-06 01:23:46 +0200

    merge fix mini calendar made by alejandro

--- src/js/calendar.js
+++ src/js/calendar.js
@@ -2,59 +2,61 @@ const locale = "us";
 const numberOfMonths = [...Array(12).keys()];
 
 function createMiniCalendar(year, previous) {
-  const miniCalendarContainer = document.getElementById(
-    "dayCard-calendar__div"
-  );
-  const mainCalendar = document.querySelectorAll(`[year = "${year}"]`);
-  if (previous) {
-    let test = Array.from(mainCalendar);
-    test.reverse().forEach((months) => {
-      let newMonth = months.cloneNode(true);
-      miniCalendarContainer.insertBefore(
-        newMonth,
-        miniCalendarContainer.firstChild
-      );
+    const miniCalendarContainer = document.getElementById(
+        "dayCard-calendar__div"
+    );
+    const mainCalendar = document.querySelectorAll(`[year = "${year}"]`);
+    if (previous) {
+        let test = Array.from(mainCalendar);
+        test.reverse().forEach((months) => {
+            let newMonth = months.cloneNode(true);
+            miniCalendarContainer.insertBefore(
+                newMonth,
+                miniCalendarContainer.firstChild
+            );
+        });
+    } else {
+        Array.from(mainCalendar).forEach((months) => {
+            let newMonth = months.cloneNode(true);
+            miniCalendarContainer.appendChild(newMonth);
+        });
+    }
+
+    const allUlElements = miniCalendarContainer.querySelectorAll(
+        ".cell__calendar-events"
+    );
+    const allPencils = miniCalendarContainer.querySelectorAll(".pencil-create");
+    const ulHeaderCell = miniCalendarContainer.querySelectorAll(
+        ".cell__calendar_eventType"
+    );
+    const headerCell = miniCalendarContainer.querySelectorAll(
+        ".dayCard-calendar__div .mainCalendar .calendar>h2"
+    );
+    const weekdays = miniCalendarContainer.querySelectorAll(
+        ".dayCard-calendar__div .days__container .weekDay"
+    );
+
+    allUlElements.forEach((el) => {
+        el.remove();
     });
-  } else {
-    Array.from(mainCalendar).forEach((months) => {
-      let newMonth = months.cloneNode(true);
-      miniCalendarContainer.appendChild(newMonth);
+
+    allPencils.forEach((el) => {
+        el.remove();
     });
-  }
 
-  const allUlElements = miniCalendarContainer.querySelectorAll(
-    ".cell__calendar-events"
-  );
-  const allPencils = miniCalendarContainer.querySelectorAll(".pencil-create");
-  const ulHeaderCell = miniCalendarContainer.querySelectorAll(
-    ".cell__calendar_eventType"
-  );
-  const headerCell = miniCalendarContainer.querySelectorAll(
-    ".dayCard-calendar__div .mainCalendar .calendar>h2"
-  );
-  const weekdays = miniCalendarContainer.querySelectorAll(
-    ".dayCard-calendar__div .days__container .weekDay"
-  );
+    ulHeaderCell.forEach((el) => {
+        el.remove();
+    });
 
-  allUlElements.forEach((el) => {
-    el.remove();
-  });
+    headerCell.forEach((el) => {
+        el.remove();
+    });
 
-  allPencils.forEach((el) => {
-    el.remove();
-  });
-
-  ulHeaderCell.forEach((el) => {
-    el.remove();
-  });
-
-  headerCell.forEach((el) => {
-    el.remove();
-  });
-
-  weekdays.forEach((weekDay) => {
-    weekDay.textContent = weekDay.textContent.substring(0, 3);
-  });
+    weekdays.forEach((weekDay) => {
+        weekDay.textContent = weekDay.textContent.substring(0, 3);
+    });
 }
 
-export { createMiniCalendar };
+export {
+    createMiniCalendar
+};
